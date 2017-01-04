916a4382bbbfc8f76c9174383045671daae57b2c
wangmeng <wangnew2013@126.com>
2017-01-04 09:38:53 +0800

    update

--- config.js
+++ config.js
@@ -1,11 +1,40 @@
 module.exports = {
   1: {
-    listenPort: 8001,
-    target: 'http://127.0.0.1:8080',
+    listen: 80,
+    target: 'http://127.0.0.1:8083',
+    ssl: false,
+    httpOptions: {
+    },
+    proxyOptions: {
+    },
+    proxyTable: [
+      {
+        origin: /niwodai.wm.dev.echo58.com/,
+        target: 'http://127.0.0.1:8082'
+      },
+      {
+        origin: /yunweili.wm.dev.echo58.com/,
+        target: 'http://127.0.0.1:8083'
+      }
+    ]
+  },
+  2: {
+    listen: 443,
+    target: 'http://127.0.0.1:8082',
     ssl: true,
     httpOptions: {
     },
     proxyOptions: {
-    }
+    },
+    proxyTable: [
+      {
+        origin: /niwodais.wm.dev.echo58.com/,
+        target: 'http://127.0.0.1:8082'
+      },
+      {
+        origin: /yunweilis.wm.dev.echo58.com/,
+        target: 'http://127.0.0.1:8083'
+      }
+    ]
   }
 }
--- src/reserve-proxy/index.js
+++ src/reserve-proxy/index.js
@@ -3,41 +3,65 @@ var https = require('https')
 var httpProxy = require('http-proxy')
 
 var fs = require('fs')
+var path = require('path')
 
-function makeProxy (config) {
+function setProxy (req, res, proxy, config) {
+  var matched = false
+  var domain = req.headers.host
+
+  for (var i = 0, len = config.proxyTable.length; i < len; i++) {
+    var item = config.proxyTable[i]
+
+    if (item.origin.test(domain)) {
+      proxy.web(req, res, {target: item.target})
+      matched = true
+      break
+    }
+  }
+  if (!matched) {
+    proxy.web(req, res, {target: config.target})
+  }
+}
+
+function startProxyServer (config, name) {
   var proxyOptions = config.proxyOptions || {}
-  console.log(proxyOptions)
   var proxy = httpProxy.createProxyServer(proxyOptions)
 
   var httpOptions = config.httpOptions || {}
-  console.log(httpOptions)
 
   var server
+  var ports = Object.prototype.toString.call(config.listen) === '[object Array]' ?
+    config.listen : [config.listen]
+
   if (config.ssl) {
     Object.assign(httpOptions, {
-      key: fs.readFileSync(config.sslKey || './secure/server.key', 'utf8'),
-      cert: fs.readFileSync(config.sslCert || './secure/server.crt', 'utf8')
+      key: fs.readFileSync(config.sslKey || path.join(__dirname, '../secure/server.key'), 'utf8'),
+      cert: fs.readFileSync(config.sslCert || path.join(__dirname, '../secure/server.crt'), 'utf8')
     })
 
-    server = https.createServer(httpOptions, function(req, res) {
-      // console.log(req.headers)
-      proxy.web(req, res, { target: config.target })
+    server = https.createServer(httpOptions, function (req, res) {
+      setProxy(req, res, proxy, config)
+    })
+
+    ports.map(function (port) {
+      console.log('Server ' + name + ' Listening at https://localhost:' + port + '\n')
+      server.listen(port)
     })
   } else {
-    server = http.createServer(httpOptions, function(req, res) {
-      // console.log(req.headers)
-      proxy.web(req, res, { target: config.target })
+    server = http.createServer(function (req, res) {
+      setProxy(req, res, proxy, config)
+    })
+
+    ports.map(function (port) {
+      console.log('Server ' + name + ' Listening at http://localhost:' + port + '\n')
+      server.listen(port)
     })
   }
-
-  var port = config.listenPort
-  console.log("listening on port " + port)
-  server.listen(port)
 }
 
-module.exports = function (configs) {
+module.exports = function start (configs) {
   for (var key in configs) {
     var config = configs[key]
-    makeProxy(config)
+    startProxyServer(config, key)
   }
 }
